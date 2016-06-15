51022a2a501a0e7f16ace9f3d627cf50b5720935
Milan Vojnovič <milanvojnovic@gmail.com>
2016-06-15 14:30:48 +0200

    variable rules examples

--- hello.py
+++ hello.py
@@ -11,3 +11,15 @@ def index():
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
